734a1b7c5f152ed711a64e5500486202ad19b344
theteleporter <m6253272@gmail.com>
2026-02-04 02:32:07 +0300

    fix: manifest.json resolved

--- app/manifest.ts
+++ app/manifest.ts
@@ -7,18 +7,20 @@ export default function manifest(): MetadataRoute.Manifest {
     description: 'Handcrafted collectible dolls where kawaii meets couture',
     start_url: '/',
     display: 'standalone',
-    background_color: '#ffffff',
+    background_color: '#FAFAFA',
     theme_color: '#000000',
     icons: [
       {
         src: '/icon-192.png',
         sizes: '192x192',
         type: 'image/png',
+        purpose: 'any maskable',
       },
       {
         src: '/icon-512.png',
         sizes: '512x512',
         type: 'image/png',
+        purpose: 'any maskable',
       },
     ],
   }
